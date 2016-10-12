8724bb9b6dcc5e714b50151733194ec2e77b3885
Unknown <cefudas@gmail.com>
2016-10-11 21:50:52 -0400

    Initial Commit

---
+++ testi/Assets/SinWave.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinWave : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localEulerAngles = new Vector3 (
+			90f,
+			10f * Mathf.Sin (Time.time),
+			2f * Mathf.Sin (Time.time * 3)
+		);
+	}
+}
